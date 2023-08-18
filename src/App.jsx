d7a3edd9cabbe9782116832771afe9c5fe77cf1c
jesus ramirez <135658843+A1L2E3J4O5@users.noreply.github.com>
2023-08-18 12:03:00 -0500

    primer-react

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+
+function App() {
+  return (
+    <div className="App">
+            <div class="container">
+        <div class="caja1 borde">
+            <h1 class="H1">JESUS ALEJANDRO RAMIREZ RUIZ</h1>
+            <ul>
+            <li>TELEFONO</li>
+                311 394 2925
+            <li>CIUDAD</li>
+                Bogota D.C
+            <li>PAIS</li>
+                Colombia
+            <li>EDAD</li>
+                21 Años
+            <li>EMAIL</li>
+                jesus.alejandroramirez.ruiz@gmail.com
+            </ul>
+            <p>Estudiante del programa Programacion full stack de Protalento. Soy una persona concentrada respetuosa me gusta hacer todo de la mejor manera posible y dar el lo mejor de mi en todo lo que hago tengo conocimiento básico en Excel, Microsoft Word y PowerPoint, programacion basica, manejo de html, css javascripty MYSQ.  Cuento con un tecnico en mantenimiento de computadores y conectividad</p>
+            <textarea name="" id="" cols="10" rows="10" > Quieres contactarme? dejame un mensaje: </textarea> 
+            <button type="submit"> ENVIAR </button>
+                
+        </div>
+        <div class="caja2 ">
+            <h2>Hobilidades tecnicas y tecnologicas</h2>
+            <table  >
+                <tr>
+                    <th>JAVASCRPIT</th>
+                    <th>HTML</th>
+                    <th>CSS</th>
+                </tr>
+                
+                    <td>Manejo Basico</td>
+                    <td>Manejo Basico</td>
+                    <td>Manejo Basico</td>
+                
+                
+            </table> 
+
+            <h2>FORMACION ACADEMICA</h2> 
+                       
+            <ul>
+                <li>PRIMARIA</li>
+                    Laureano Gomez IED 2011 - 2015
+                <li>SECUNDARIA</li>
+                    Fundacion San Marino 2020
+                <li>TECNICO</li>
+                    Inesco 2021 - actualidad
+            </ul>
+
+        </div>
+        <div class="caja3 borde1">
+            <h2>EXPERIENCIA</h2> 
+                       
+            <ul><li>STI SAS</li>
+                PRACTICANTE TECNICO EN REPARACION DE COMPUTADORES Y CONECTIVIDAD, APOYO EN EL AREA DE IT, APOYO EN EL AREA DE BODEGA  2022 - 2023
+            </ul>
+            <h2>Intereses</h2>
+        <table >
+            <tr>
+                <th>Desarrolo Web</th>
+                <th> Desarrollo de Aplicaciones moviles </th>
+                <th>SQL</th>
+            </tr>
+            
+                <td>Poder crear paginas total mente funcionales</td>
+                <td> Tener el conocimiento necesario para crear aplicaciones que ayuden a las personas en su dia a dia</td>
+                <td>Manejar y administrar bases de datos</td>
+            
+        </table>
+        </div>
+        <div class="caja4 borde1">
+            <div class="imagen">
+                <p>MI GITHUB</p> 
+            </div> 
+            <div class="imagen2">
+             </div>
+        </div>
+    </div>
+
+    </div>
+  );
+}
+
+export default App
